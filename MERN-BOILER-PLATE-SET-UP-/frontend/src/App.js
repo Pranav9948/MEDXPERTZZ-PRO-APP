fdaa887df4bb94219891e457df8b23dcb94495de
Pranav P <pranavat9948@gmail.com>
2023-06-17 15:25:03 +0530

    medXperts initial set up completed

---
+++ MERN-BOILER-PLATE-SET-UP-/frontend/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+  return (
+    <div className="App">
+       <div className="App">
+      <div className="fixed-header">
+        <header>
+
+        <h1 class="text-3xl font-bold underline">
+        Doctor App
+      </h1>
+
+
+        </header>
+      </div>
+      <div className="content-wrap">
+        <main className="py-4" style={{ marginTop: '150px' }}>
+          <Outlet />
+        </main>
+      </div>
+      <footer className="fixed-footer" style={{ marginTop: 'calc(100vh - 300px)' }}>
+        
+      </footer>
+      <ToastContainer />
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ MERN-BOILER-PLATE-SET-UP-/frontend/src/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      {/* 
+      private Routes component  */}
+
+      {/* 
+          AdminRoutes component */}
+    </Route>
+  )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
+
+
